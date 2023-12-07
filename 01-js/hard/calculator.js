3671a50bbc212281d5a883029012244dbbbab43a
amit <amit63390@gmail.com>
2023-12-07 14:53:09 +0530

    Hard problems

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -1,21 +1,57 @@
-/*
-  Implement a class `Calculator` having below methods
-    - initialise a result variable in the constructor and keep updating it after every arithmetic operation
-    - add: takes a number and adds it to the result
-    - subtract: takes a number and subtracts it from the result
-    - multiply: takes a number and multiply it to the result
-    - divide: takes a number and divide it to the result
-    - clear: makes the `result` variable to 0
-    - getResult: returns the value of `result` variable
-    - calculate: takes a string expression which can take multi-arithmetic operations and give its result
-      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
-      Points to Note: 
-        1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
-        2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
 
-  Once you've implemented the logic, test your code by running
-*/
+  add(num) {
+    this.result += num;
+    return this.result;
+  }
 
-class Calculator {}
+  subtract(num) {
+    this.result -= num;
+    return this.result;
+  }
+
+  multiply(num) {
+    this.result *= num;
+    return this.result;
+  }
+
+  divide(num) {
+    if (num === 0) {
+      throw new Error('Division by zero is not allowed.');
+    }
+    this.result /= num;
+    return this.result;
+  }
+  
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    const validExpression = /^[0-9+*\-/().\s]+$/;
+    if (!validExpression.test(expression)) {
+      throw new Error('Invalid expression');
+    }
+
+    // Use a library or implement an expression parser to evaluate the expression
+    // Example using eval (not recommended for production due to security risks)
+    try {
+      const sanitizedExpression = expression.replace(/\s+/g, ''); // Remove spaces
+      const result = eval(sanitizedExpression); // Evaluate the expression
+      this.result = result; // Update the calculator's result
+      return result;
+    } catch (error) {
+      throw new Error('Invalid expression');
+    }
+  }
+}
 
 module.exports = Calculator;
